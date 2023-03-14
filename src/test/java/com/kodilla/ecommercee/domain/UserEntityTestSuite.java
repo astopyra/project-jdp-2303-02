ae46601a74b79d4e5c3d3af31f52a761d8d7b7e1
Jan Sikorski <jans3876@gmail.com>
2023-03-15 00:18:45 +0100

    Testy dla encji user napisane; Niestety konieczne było usunięcie @Not null z tabeli encji by mógł test działać

--- src/test/java/com/kodilla/ecommercee/domain/UserEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/UserEntityTestSuite.java
@@ -1,36 +1,102 @@
 package com.kodilla.ecommercee.domain;
 
+import com.kodilla.ecommercee.repository.CartRepository;
 import com.kodilla.ecommercee.repository.UserRepository;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
+@Transactional
+@RunWith(SpringRunner.class)
 public class UserEntityTestSuite {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private CartRepository cartRepository;
 
     @Test
-    public void testUserEntitySave() {
+    public void testUserFindAll() {
+        //Given & When
+        userRepository.save(new User());
+        userRepository.save(new User());
+        userRepository.save(new User());
+
+        //Then
+        assertEquals(3, userRepository.findAll().size());
+    }
+
+    @Test
+    public void testSaveUser() {
+        //Given & When
+        userRepository.save(new User());
+        userRepository.save(new User());
+        userRepository.save(new User());
+
+        //Then
+        assertEquals(3, userRepository.findAll().size());
+    }
+
+    @Test
+    public void testDeleteUserById() {
         //Given
-        User user = new User();
+        User user1 = new User();
+        User user2 = new User();
+        User user3 = new User();
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
 
         //When
-        List<User> users = userRepository.findAll();
+        int sizeBeforeDelete = userRepository.findAll().size();
+        userRepository.deleteById(user2.getId());
+        int sizeAfterDelete = userRepository.findAll().size();
+
+        //Then
+        assertEquals(3, sizeBeforeDelete);
+        assertEquals(2, sizeAfterDelete);
+    }
+
+    @Test
+    public void testFindUserById() {
+        //Given & When
+        User user1 = new User();
+        User user2 = new User();
+        User user3 = new User();
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+
+        //Then
+        assertEquals(user1, userRepository.findById(user1.getId()).orElse(null));
+    }
+
+    @Test
+    public void testCreateuserWithCarts() {
+        //Given
+        User user = new User();
+        Cart cart1 = new Cart();
+        Cart cart2 = new Cart();
+
+        //When
+        user.getCarts().add(cart1);
+        user.getCarts().add(cart2);
         userRepository.save(user);
 
         //Then
-        assertEquals(users.size()+1, userRepository.findAll().size());
-
-        //Cleanup
-        userRepository.delete(new User());
+        assertEquals(1, userRepository.findAll().size());
+        assertEquals(2, cartRepository.findAll().size());
     }
-
-
 }
